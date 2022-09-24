56e2c4a3141e2da752e10e3e8d4a887e32f5869f
Edgar <avila.agramonte.edg@gmail.com>
2022-09-24 15:51:34 -0500

    Parser eval unit tests

--- src/parser.rs
+++ src/parser.rs
@@ -71,3 +71,51 @@ impl<'a> Parser<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Parser;
+    use crate::Lexer;
+
+    fn test(s: &str, expected: bool) {
+        let tokens = Lexer::new(s.chars()).get_tokens();
+        let mut parser = Parser::new(tokens.iter());
+        assert_eq!(parser.parse().eval(), expected);
+    }
+
+    #[test]
+    fn single_bool() {
+        test("true", true);
+        test("false", false);
+    }
+
+    #[test]
+    fn binary_and() {
+        test("true and true", true);
+        test("true and false", false);
+        test("false and true", false);
+        test("false and false", false);
+    }
+
+    #[test]
+    fn binary_or() {
+        test("true or true", true);
+        test("true or false", true);
+        test("false or true", true);
+        test("false or false", false);
+    }
+
+    #[test]
+    fn associativity() {
+        test("true or true and false", false);
+        test("false and false or true", true);
+    }
+
+    #[test]
+    fn expr_with_parentheses() {
+        test("(true or true) and false", false);
+        test("(false and false) or true", true);
+        test("true or (true and false)", true);
+        test("false and (false or true)", false);
+    }
+}
