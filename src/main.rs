0a3abdcf0248bad5221ee216c3ff1b2d79668307
Edgar Avila <avila.agramonte.edg@gmail.com>
2022-09-26 19:52:48 -0500

    Handling user input

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
-mod token;
 mod lexer;
 mod nodes;
 mod parser;
+mod token;
+
+use std::io::{self, Write};
 
 use crate::lexer::Lexer;
 use crate::parser::Parser;
 
 fn main() {
-    let code = String::from("false or (true or false) and true");
+    loop {
+        // Read input
+        print!(">>> ");
+        io::stdout().flush().expect("Unable to flush stdout");
+        let mut code = String::new();
+        io::stdin()
+            .read_line(&mut code)
+            .expect("Could not read input");
 
-    // Lexer
-    let mut l = Lexer::new(code.chars());
-    let tokens = l.get_tokens();
-    println!("The tokens are: {:?}", tokens);
+        // Lexer
+        let mut l = Lexer::new(code.chars());
+        let tokens = l.get_tokens();
+        println!("The tokens are: {:?}", tokens);
 
-    // Parser
-    let mut p = Parser::new(tokens.iter());
-    let ast = p.parse();
-    println!("The ast generated evaluates the expression as follows: {}", ast);
+        // Parser
+        let mut p = Parser::new(tokens.iter());
+        let ast = p.parse();
+        println!(
+            "The ast generated evaluates the expression as follows: {}",
+            ast
+        );
 
-    // Show final resutl
-    let value = ast.eval();
-    println!("The value evaluated is: {}", value);
+        // Show final resutl
+        let value = ast.eval();
+        println!("The value evaluated is: {}", value);
+    }
 }
